3b001e879b8fed0d810f76f4f75f21e240b78d09
Jeffser <jsamuel@tuta.io>
2026-07-19 02:23:23 -0600

    fix stutter when playing big queues

--- src/widgets/playing/queue_page.py
+++ src/widgets/playing/queue_page.py
@@ -32,21 +32,18 @@ class PlayingQueuePage(Gtk.ScrolledWindow):
         global_queue = integration.loaded_models.get('currentSong').get_property('queueModel')
         if len(list(self.song_list_el.list_el)) == 0:
             self.queue_changed(global_queue, 0, 0, global_queue.get_property('n-items'))
-        global_queue.connect('items-changed', self.queue_changed)
+        global_queue.connect('items-changed', lambda *args: GLib.idle_add(self.queue_changed, *args))
 
     def queue_changed(self, global_queue, position:int, removed:int, added:int):
         for _ in range(removed):
             if row := self.song_list_el.list_el.get_row_at_index(position):
                 self.song_list_el.list_el.remove(row)
 
-        def run():
-            for i in range(added):
-                if item := global_queue.get_item(position + i):
-                    row = SongRow(
-                        item.get_string(),
-                        draggable=True,
-                        removable=True
-                    )
-                    GLib.idle_add(self.song_list_el.list_el.insert, row, position + i)
-        threading.Thread(target=run, daemon=True).start()
-
+        for i in range(added):
+            if item := global_queue.get_item(position + i):
+                row = SongRow(
+                    item.get_string(),
+                    draggable=True,
+                    removable=True
+                )
+                self.song_list_el.list_el.insert(row, position + i)
